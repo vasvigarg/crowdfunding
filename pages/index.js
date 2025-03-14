73c4c14b5335df61821a1dc0c18088ea4499601a
Vasvi Garg <gargvasvi2004@gmail.com>
2025-03-14 14:31:25 +0530

    tailwind setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
 };
 
 export default index;
